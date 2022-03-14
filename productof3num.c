3566c8828ce743c3b171453698ceb46e952e09c1
Somin <sominupadhyaya@gmail.com>
2022-03-14 21:35:13 +0545

    Completed the code

--- productof3num.c
+++ productof3num.c
@@ -3,7 +3,7 @@
 int main()
 {
     int a,b,c,product;
-    printf("Enter any 3 numbers");
+    printf("Enter any 3 numbers \n");
     scanf("%d %d %d" , &a , &b ,&c);
     product = a*b*c;
     printf("The product is : %d" , product);
